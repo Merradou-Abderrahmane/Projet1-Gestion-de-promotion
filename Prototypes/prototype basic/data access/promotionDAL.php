4c8d3b934bd42c4d494febfa4c9eebea43d663e3
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2022-10-11 14:58:52 +0100

    insert method

---
+++ Prototypes/prototype basic/data access/promotionDAL.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'mysqlconnection.php';
+require_once 'promotion.php';
+
+class PromotionDAL extends Connection {
+ 
+    public function addPromotion($promotion){
+
+        $name = $promotion->getName();
+        // Sql query
+        $insertRow="INSERT INTO promotion (name) VALUES ('$name')";
+                          
+        // Execute query
+        mysqli_query($this->Connect(), $insertRow);
+    }
+
+
+}
